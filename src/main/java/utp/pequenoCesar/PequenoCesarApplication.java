2e719a5a67d0173670f40fbaf07f2abdfa0dccd8
Wilson Giovanni Torres Huaman <lokiwilson2002@gmail.com>
2026-04-15 00:19:14 -0500

    primer commit

---
+++ src/main/java/utp/pequenoCesar/PequenoCesarApplication.java
@@ -0,0 +1,13 @@
+package utp.pequenoCesar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PequenoCesarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PequenoCesarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/utp/pequenoCesar/PequenoCesarApplicationTests.java
@@ -0,0 +1,13 @@
+package utp.pequenoCesar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PequenoCesarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
